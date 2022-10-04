23514e76eccc7fbae6c63340b06ec7328255c721
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-04 18:30:06 -0300

    feat: register screen basic layout

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,5 +1,77 @@
-function Register(params) {
-    return <></>
+import styled from "styled-components";
+import { useState } from "react";
+
+function Register() {
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+  
+    return (
+    <Content>
+      <LeftContainer>
+      <form onSubmit={()=>{}}>
+        <input
+            type="email" 
+            placeholder="e-mail"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+            type="password"
+            placeholder="password"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+            type="text"
+            placeholder="username"
+            value={userName}
+            required
+            onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+            type="text"
+            placeholder="confirm your password"
+            value={confirmPassword}
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+          <button type="submit">
+            Register
+          </button>
+        </form>
+      </LeftContainer>
+      <RightContainer>
+
+      </RightContainer>
+    </Content>
+  );
 }
+
+const Content = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+const LeftContainer = styled.div`
+  width: 600px;
+  height: 100%;
+  padding: 40px;
+  background-color: white;
+  form{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+const RightContainer = styled.div`
+  width: auto;
+  height: 100%;
+  background-color: black;
+`;
+
 
 export default Register;
