def155705298c31fd0ea64221ba4baf826f5e993
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-07 15:57:19 -0300

    feat: create post component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import styled from "styled-components"
 import Header from "../../components/Header"
 
 
 export default function Home() {
+    const [token, setToken] = useState(localStorage.getItem("authToken"));
+    
+    console.log(token)
+
+
+    
     return(
         <>
         <Header/>
+        <Content>
+            <PostsBox>
+
+            </PostsBox>
+        </Content>
         </>
     )
 };
+
+const Content = styled.div``
+
+const PostsBox = styled.div``
