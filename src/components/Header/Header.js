c4935f32230d7997e2ac9491165ae47f19717724
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-07 15:46:11 -0300

    feat: Header basic

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export default function Header() {
+    return (
+        <Container>
+        <h1>Gambit</h1>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 55px;
+    background-color: grey;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 15px;
+
+    h1{
+        font-size: 27px;
+    }
+
+`
